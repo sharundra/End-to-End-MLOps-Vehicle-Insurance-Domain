944293c5a8d52d65b07ea7bf50966beaa251066e
sharundra <sharadkrishna510@gmail.com>
2025-11-05 23:57:58 +0530

    work in progress

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sharad Mishra",
+    author_email="sharadkrishna510@gmail.com",
+    packages=find_packages()
+)
